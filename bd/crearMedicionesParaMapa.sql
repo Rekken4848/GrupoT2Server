3ec7bf58e7a6e663f8f0f87a44028fdda64cacba
Rekken4848 <hugomarescrihuela@gmail.com>
2023-12-11 00:02:22 +0100

    Arreglos mapa funcional completo

--- bd/crearMedicionesParaMapa.sql
+++ bd/crearMedicionesParaMapa.sql
@@ -1,5 +1,9 @@
---crearMedicionesParaMapa.sql
+--crearMedicionesParaMapa.
 
+insert into Persona (dni, nombre, apellidos, correo, telefono) values ( 'PruebaMapa', 'Mario', 'Casas', 'mariocasas@gmail.com', '999999999');
+insert into Dispositivo (dispositivo_id, dni_empleado) values ( 'PrueMapaDis', 'PruebaMapa');
+
+--insert into TipoValor (tipo_valor) values ('CO2');
 insert into TipoValor (tipo_valor) values ('CO3');
 insert into TipoValor (tipo_valor) values ('N');
 insert into TipoValor (tipo_valor) values ('C8');
@@ -16,3 +20,17 @@ insert into Medicion (valor, tipo_valor_id, fecha, lugar) values ( 32.34, 1, '20
 insert into Medicion (valor, tipo_valor_id, fecha, lugar) values ( 42.34, 1, '2023-10-15 16:32:40', '38.994357, -0.160923');
 insert into Medicion (valor, tipo_valor_id, fecha, lugar) values ( 06.34, 2, '2023-10-15 16:32:40', '38.971025, -0.169295');
 insert into Medicion (valor, tipo_valor_id, fecha, lugar) values ( 15.34, 3, '2023-10-15 16:32:40', '38.969066, -0.170063');
+
+
+insert into Medicion_Dispositivo (medicion_id, dispositivo_id) values ( 1, 'PrueMapaDis');
+insert into Medicion_Dispositivo (medicion_id, dispositivo_id) values ( 2, 'PrueMapaDis');
+insert into Medicion_Dispositivo (medicion_id, dispositivo_id) values ( 3, 'PrueMapaDis');
+insert into Medicion_Dispositivo (medicion_id, dispositivo_id) values ( 4, 'PrueMapaDis');
+insert into Medicion_Dispositivo (medicion_id, dispositivo_id) values ( 5, 'PrueMapaDis');
+insert into Medicion_Dispositivo (medicion_id, dispositivo_id) values ( 6, 'PrueMapaDis');
+insert into Medicion_Dispositivo (medicion_id, dispositivo_id) values ( 7, 'PrueMapaDis');
+insert into Medicion_Dispositivo (medicion_id, dispositivo_id) values ( 8, 'PrueMapaDis');
+insert into Medicion_Dispositivo (medicion_id, dispositivo_id) values ( 9, 'PrueMapaDis');
+insert into Medicion_Dispositivo (medicion_id, dispositivo_id) values ( 10, 'PrueMapaDis');
+insert into Medicion_Dispositivo (medicion_id, dispositivo_id) values ( 11, 'PrueMapaDis');
+insert into Medicion_Dispositivo (medicion_id, dispositivo_id) values ( 12, 'PrueMapaDis');
--- ux/mapa.js
+++ ux/mapa.js
@@ -1,5 +1,5 @@
 function getCustomIcon(type) {
-    const color = getColor(type);
+    const color = getColorMarcador(type);
     return L.divIcon({
         className: 'custom-icon',
         iconSize: [20, 20],
@@ -16,13 +16,25 @@ function addToLegend(type) {
     legendContent.appendChild(entry);
 }
 
-function getColor(type) {
+function getColorMarcador(nivel) {
     // Asigna colores diferentes según el tipo de medición
-    return type === 1 ? 'red' : type === 2 ? 'blue' : type === 3 ? 'yellow' : type === 4 ? 'pink' : 'green';
+    return nivel === 'alto' ? 'red' : nivel === 'medio' ? 'yellow' : nivel === 'bajo' ? 'green' : 'blue';
 }
 
+function getColor(type) {
+    // Asigna colores diferentes según el tipo de medición
+    return type === 'CO2' ? 'red' : type === 'CO3' ? 'blue' : type === 'N' ? 'yellow' : type === 'C8' ? 'pink' : 'green';
+}
+
+let mymap;
+
 function generarYGenerarMapa() {
-    const mymap = L.map('zonaMapa').setView([51.505, -0.09], 13);
+    // Eliminar el mapa si ya está inicializado
+    if (mymap) {
+        mymap.remove();
+    }
+
+    mymap = L.map('zonaMapa').setView([51.505, -0.09], 13);
 
     // Añade un mosaico de OpenStreetMap al mapa
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -36,8 +48,32 @@ function generarYGenerarMapa() {
         .bindPopup('¡Hola, mundo!').openPopup();
 }
 
+// Función para obtener el color según la clasificación
+function getColorByClasificacion(clasificacion) {
+    // Define colores según la clasificación
+    const colores = {
+        alto: 'red',
+        medio: 'yellow',
+        bajo: 'green'
+    };
+    return colores[clasificacion] || 'gray'; // Color por defecto
+}
+
+// Función para obtener la clasificación según el valor
+function getClasificacion(valor) {
+    const umbralAlto = 30;
+    const umbralMedio = 15;
+    if (valor > umbralAlto) {
+        return 'alto';
+    } else if (valor > umbralMedio) {
+        return 'medio';
+    } else {
+        return 'bajo';
+    }
+}
+
 function mostrarLeyendaYMarcadores(mymap) {
-    fetch('http://localhost:8080/todasMediciones', {
+    fetch('http://localhost:8080/medicionConTipoValorEntreFechas/2023-10-14 16:32:40/2023-10-16 16:32:40', {
         method: "GET"
     }).then(function (respuesta) {
 
@@ -56,32 +92,88 @@ function mostrarLeyendaYMarcadores(mymap) {
         // Crea el control de capas y añádelo al mapa
         const controlCapas = L.control.layers(null, capas).addTo(mymap);
 
+        const alto = [];
+        const medio = [];
+        const bajo = [];
+
+        const umbralAlto = 30;
+        const umbralMedio = 15;
+
         datos.forEach(markerData => {
-            const tipo = `Tipo ${markerData.tipo_valor_id}`;
+            const tipo = `${markerData.tipo_valor}`;
+            // ... (resto del código)
+
+            // Clasifica la medición en la lista correspondiente
+            if (markerData.valor > umbralAlto) {
+                alto.push(markerData);
+            } else if (markerData.valor > umbralMedio) {
+                medio.push(markerData);
+            } else {
+                bajo.push(markerData);
+            }
+        });
+
+        datos.forEach(markerData => {
+            const tipo = `${markerData.tipo_valor}`;
             if (!capas[tipo]) {
                 capas[tipo] = L.layerGroup(); // Crea una capa de grupo para cada tipo de dato
-                const cuadroColor = `<span style="display:inline-block; width: 12px; height: 12px; background-color: ${getColor(Number(tipo.split(' ')[1]))}; margin-right: 5px;"></span>`;
+                console.log("Número de tipo_valor:", Number(tipo.split(' ')[1]));
+                const cuadroColor = `<span style="display:inline-block; width: 12px; height: 12px; background-color: ${getColor(markerData.tipo_valor)}; margin-right: 5px;"></span>`;
                 controlCapas.addOverlay(capas[tipo], `${cuadroColor} ${tipo}`);
             }
+        });
+
+        console.log("Alto: " + JSON.stringify(alto));
+        console.log("Medio: " + JSON.stringify(medio));
+        console.log("Bajo: " + JSON.stringify(bajo));
+
+        alto.forEach(markerData => {
+            const tipo = `${markerData.tipo_valor}`;
+            
             // Dividir la cadena 'lugar' en latitud y longitud
             const [latitud, longitud] = markerData.lugar.split(',').map(parseFloat);
 
             console.log("Lat: " + latitud + ", Lon: " + longitud);
-            const marker = L.marker([latitud, longitud], { icon: getCustomIcon(markerData.tipo_valor_id) }).addTo(mymap);
-
-            // Personaliza el marcador según el tipo de medición
-            marker.bindPopup(`Tipo: ${markerData.tipo_valor_id}<br>Valor: ${markerData.valor}`);
-            addToLegend(markerData.tipo_valor_id);
+            const marker = L.marker([latitud, longitud], { icon: getCustomIcon('alto') }).addTo(mymap);
+            marker.bindPopup(`Tipo: ${markerData.tipo_valor}<br>Valor: ${markerData.valor}`);
+            addToLegend(markerData.tipo_valor);
+            //marker.setIcon(getCustomIcon(markerData.tipo_valor, 'red'));
 
             // Añade el marcador a la capa correspondiente
             capas[tipo].addLayer(marker);
         });
 
-        // Agrega cuadros de color junto a cada tipo en el control de capas
-        /*for (const tipo in capas) {
-            const cuadroColor = `<span style="display:inline-block; width: 12px; height: 12px; background-color: ${getColor(Number(tipo.split(' ')[1]))}; margin-right: 5px;"></span>`;
-            controlCapas.addOverlay(capas[tipo], `${cuadroColor} ${tipo}`);
-        }*/
+        medio.forEach(markerData => {
+            const tipo = `${markerData.tipo_valor}`;
+            
+            // Dividir la cadena 'lugar' en latitud y longitud
+            const [latitud, longitud] = markerData.lugar.split(',').map(parseFloat);
+
+            console.log("Lat: " + latitud + ", Lon: " + longitud);
+            const marker = L.marker([latitud, longitud], { icon: getCustomIcon('medio') }).addTo(mymap);
+            marker.bindPopup(`Tipo: ${markerData.tipo_valor}<br>Valor: ${markerData.valor}`);
+            addToLegend(markerData.tipo_valor);
+            //marker.setIcon(getCustomIcon(markerData.tipo_valor, 'yellow'));
+
+            // Añade el marcador a la capa correspondiente
+            capas[tipo].addLayer(marker);
+        });
+
+        bajo.forEach(markerData => {
+            const tipo = `${markerData.tipo_valor}`;
+            
+            // Dividir la cadena 'lugar' en latitud y longitud
+            const [latitud, longitud] = markerData.lugar.split(',').map(parseFloat);
+
+            console.log("Lat: " + latitud + ", Lon: " + longitud);
+            const marker = L.marker([latitud, longitud], { icon: getCustomIcon('bajo') }).addTo(mymap);
+            marker.bindPopup(`Tipo: ${markerData.tipo_valor}<br>Valor: ${markerData.valor}`);
+            addToLegend(markerData.tipo_valor);
+            //marker.setIcon(getCustomIcon(markerData.tipo_valor, 'green'));
+
+            // Añade el marcador a la capa correspondiente
+            capas[tipo].addLayer(marker);
+        });
     })
 }
 
