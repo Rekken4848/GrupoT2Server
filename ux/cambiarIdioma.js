f6e61e14ba1316f1085b14803dd3a2faff8b0e7e
Diego Baldoví <diegobal007@gmail.com>
2023-12-12 16:20:02 +0100

    Idioma Ajustado

--- ux/cambiarIdioma.js
+++ ux/cambiarIdioma.js
@@ -4,7 +4,6 @@ function getTextos(){
     textosACambiar = document.querySelectorAll("[data-value]")
     console.log(textosACambiar.length)
 
-    cambiaIdioma("es")
 }
 
 async function cambiaIdioma(languaje){
@@ -18,20 +17,18 @@ async function cambiaIdioma(languaje){
 
         if (valor.includes("infoContaminante")){
             if(valor.includes("Ozono")){
-                elementoACambiar.innerHTML = <img class="inset" src="images/ozono.png" alt="" /> + texts[section][valor]
+                elementoACambiar.innerHTML = '<img class="inset" src="images/ozono.png" alt="" />' + texts[section][valor]
             } else if (valor.includes("Nitrogeno")) {
-                elementoACambiar.innerHTML = <img class="inset" src="images/nitrogeno.png" alt="" /> + texts[section][valor]
+                elementoACambiar.innerHTML = '<img class="inset" src="images/nitrogeno.png" alt="" />' + texts[section][valor]
             } else if (valor.includes("Azufre")) {
-                elementoACambiar.innerHTML = <img class="inset" src="images/azufre.png" alt="" /> + texts[section][valor]
+                elementoACambiar.innerHTML = '<img class="inset" src="images/azufre.png" alt="" />' + texts[section][valor]
             } else if (valor.includes("Carbono")) {
-                elementoACambiar.innerHTML = <img class="inset" src="images/carbon.png" alt="" /> + texts[section][valor]
+                elementoACambiar.innerHTML = '<img class="inset" src="images/carbon.png" alt="" />' + texts[section][valor]
             } else if (valor.includes("Benceno")) {
-                elementoACambiar.innerHTML = <img class="inset" src="images/benceno.png" alt="" /> + texts[section][valor]
+                elementoACambiar.innerHTML = '<img class="inset" src="images/benceno.png" alt="" />' + texts[section][valor]
             }
         } else{
             elementoACambiar.innerHTML=texts[section][valor]
         }
-        
     })
-
 }
